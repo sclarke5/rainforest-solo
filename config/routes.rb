81a6cc3ec265951d06fd64256a9d7ffa9971d553
Sclarke5 <sclarke5555@gmail.com>
2018-04-20 16:13:33 -0400

    login complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i(new create)
+  resource :users, only: %i(new create)
 
   resource :sessions, only: %i(new create destroy)
 
